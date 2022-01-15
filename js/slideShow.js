d24af1e2ee5883f40d70e3599090a9639447d231
Ibra Nurlette <sjsdev02@gmail.com>
2022-01-15 23:35:37 +0700

    some-update

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,20 +1,47 @@
 var slideIndex = 1;
 showSlides(slideIndex);
+showSlidesWork(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
+}
+function plusSlidesWork(n) {
+  showSlidesWork((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
+function currentSlideWork(n) {
+  showSlidesWork((slideIndex = n));
+}
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function showSlidesWork(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides-work");
+  var dots = document.getElementsByClassName("dot-work");
   if (n > slides.length) {
     slideIndex = 1;
   }
